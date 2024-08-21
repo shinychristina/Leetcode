fa0641725a7de0d46fd3a3dbb14b74043cddc185
Shiny Christina V <98268558+shinychristina@users.noreply.github.com>
2024-08-21 20:44:58 +0530

    Time: 515 ms (50.08%), Space: 0B (100%) - LeetHub

--- 1161-project-employees-i/1161-project-employees-i.sql
+++ 1161-project-employees-i/1161-project-employees-i.sql
@@ -1,4 +1,4 @@
-select p.project_id, round(sum(e.experience_years)/count(p.employee_id),2) as average_years
+select p.project_id, round(avg(e.experience_years),2) as average_years
 from project p
 left join employee e
 on  p.employee_id=e.employee_id
